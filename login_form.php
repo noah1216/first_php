b438d8cd8460fb35ac4abc8faff78da2c8c4c98f
Your Name <you@example.com>
2021-05-20 17:01:00 +0900

    loginページ

---
+++ login_form.php
@@ -0,0 +1,12 @@
+<h1>login</h1>
+<form action="login.php" method="post">
+<div>
+    <label>name：<label>
+    <input type="text" name="name" required>
+</div>
+<div>
+    <label>password：<label>
+    <input type="password" name="pass" required>
+</div>
+<input type="submit" value="login">
+</form>
